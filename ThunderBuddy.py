7609b48a051f84cf23ebe2da96dde4c5aded1cd5
Nick White <britishnickk@gmail.com>
2016-04-05 21:08:04 -0500

    Can now send messages through Twilio. #2

--- ThunderBuddy.py
+++ ThunderBuddy.py
@@ -1,6 +1,7 @@
 import config
 from urllib.request import urlopen
 import json
+from twilio.rest import TwilioRestClient
 
 # Retrieve Weather Underground Data
 f = urlopen("http://api.wunderground.com/api/" + config.WEATHER_UNDERGROUND_API_KEY + "/forecast/q/KS/Salina.json")
@@ -13,5 +14,9 @@ for forecastEntry in forecastList:
     if "thunder" in forecastEntry or "thunder" in fcttext_metric:
         print("Thunder coming on " + forecastEntry["title"] + "!")
 
+client = TwilioRestClient(config.TWILIO_ACCOUNT_KEY, config.TWILIO_AUTH_KEY)
+
+message = client.messages.create(to=config.RECIPIENT, from_=config.TWILIO_NUMBER,
+                                 body="Hello there!")
 # Send SMS alerts
 f.close()
