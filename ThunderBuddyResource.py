425fc490b392790407b9326a936d6aabd6ceca2a
Nick White <britishnickk@gmail.com>
2016-04-06 14:39:51 -0500

    Subscribe now works. Fixes #7

--- ThunderBuddyResource.py
+++ ThunderBuddyResource.py
@@ -8,12 +8,41 @@ cur = conn.cursor()
 cur.execute("SELECT * FROM user")
 for r in cur:
     print(r)
-cur.close()
-conn.close()
 
 @app.route("/api/subscribe/number/<number>")
 def subscribe(number):
-    return "Hello World! - " + str(number)
+    try:
+        if len(str(int(number)))>15:
+            return "Please input a valid number"
+    except:
+            return "Please input a valid number"
+    city="Austin"
+    state="TX"
+    sql = "INSERT INTO user(number,city,state) VALUES(%s,%s,%s)"
+    v = (str(number),str(city),str(state))
+    cur.execute(sql,v)
+    conn.commit()
+    return "Hello! - " + str(number)
+
+@app.route("/api/unsubscribe/number/<number>")
+def unsubscribe(number):
+    try:
+        if len(str(int(number)))>15:
+            return "Please input a valid number"
+    except:
+            return "Please input a valid number"
+    print("About to remove "+str(number)+" from user")
+    cur.execute("DELETE FROM user WHERE number="+number)
+    conn.commit()
+    print("User now contains: ")
+    cur.execute("SELECT * FROM user")
+    for r in cur:
+        print(r)
+
+    return "Goodbye! - " + str(number)
 
 if __name__ == "__main__":
     app.run()
+
+cur.close()
+conn.close()
