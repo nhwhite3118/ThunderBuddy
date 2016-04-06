3b4912bdfa82021c223f2d2e912d8acddab3f360
Nick White <britishnickk@gmail.com>
2016-04-05 20:10:57 -0500

    Sucessfully gets data from weatherunderground. Fixes #1

--- ThunderBuddy.py
+++ ThunderBuddy.py
@@ -1,3 +1,12 @@
 import config
+#import urllib2
+from urllib.request import urlopen
+import json
 
-print("example getting secret token " + config.SUPER_SECRET_TOKEN)
+f = urlopen('http://api.wunderground.com/api/'+config.WEATHER_UNDERGROUND_API_KEY+'/geolookup/conditions/q/IA/Cedar_Rapids.json')
+json_string = f.read().decode('utf-8')
+parsed_json = json.loads(json_string)
+location = parsed_json['location']['city']
+temp_f = parsed_json['current_observation']['temp_f']
+print ("Current temperature in %s is: %s" % (location, temp_f))
+f.close()
