21d772b55302a24fc21e5e06dfcd4048de669980
danielrobertson <danielrobertson733@gmail.com>
2016-04-05 20:26:36 -0500

    Extracted thunderstorm entries - testing with Kansas for now. #1

--- ThunderBuddy.py
+++ ThunderBuddy.py
@@ -1,12 +1,17 @@
 import config
-#import urllib2
 from urllib.request import urlopen
 import json
 
-f = urlopen('http://api.wunderground.com/api/'+config.WEATHER_UNDERGROUND_API_KEY+'/geolookup/conditions/q/IA/Cedar_Rapids.json')
+# Retrieve Weather Underground Data
+f = urlopen("http://api.wunderground.com/api/" + config.WEATHER_UNDERGROUND_API_KEY + "/forecast/q/KS/Salina.json")
 json_string = f.read().decode('utf-8')
 parsed_json = json.loads(json_string)
-location = parsed_json['location']['city']
-temp_f = parsed_json['current_observation']['temp_f']
-print ("Current temperature in %s is: %s" % (location, temp_f))
+forecastList = parsed_json["forecast"]["txt_forecast"]["forecastday"]
+for forecastEntry in forecastList:
+    fcttext = forecastEntry["fcttext"].lower()
+    fcttext_metric = forecastEntry["fcttext_metric"].lower()
+    if "thunder" in forecastEntry or "thunder" in fcttext_metric:
+        print("Thunder coming on " + forecastEntry["title"] + "!")
+
+# Send SMS alerts
 f.close()
