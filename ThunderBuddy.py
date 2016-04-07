16a914d679eea3c13e3b516bd63d0b6d66c49232
danielrobertson <danielrobertson733@gmail.com>
2016-04-06 19:18:32 -0500

    no changes, just formatted code

--- ThunderBuddy.py
+++ ThunderBuddy.py
@@ -5,19 +5,20 @@ from twilio.rest import TwilioRestClient
 import pymysql
 import time
 
-#We need to keep track of the number of unique cities we ask weatherunderground
-#for. We get 10 API calls/minute, and 500/day, so we need to keep totals
+# We need to keep track of the number of unique cities we ask weatherunderground
+# for. We get 10 API calls/minute, and 500/day, so we need to keep totals
 wunderground_this_day = 0
-#We need to cache cities so that our usage scales with unique cities instead
-#of unique users
+# We need to cache cities so that our usage scales with unique cities instead
+# of unique users
 forecasts = {}
 client = TwilioRestClient(config.TWILIO_ACCOUNT_KEY, config.TWILIO_AUTH_KEY)
 conn = pymysql.connect(host='127.0.0.1', user=config.DB_USER, passwd=config.DB_PASSWORD, db='thunderbuddy')
 
 
-def make_forecast(city,state):
+def make_forecast(city, state):
     # Retrieve Weather Underground Data
-    f = urlopen("http://api.wunderground.com/api/" + config.WEATHER_UNDERGROUND_API_KEY + "/forecast/q/"+state+"/"+city+".json")
+    f = urlopen(
+        "http://api.wunderground.com/api/" + config.WEATHER_UNDERGROUND_API_KEY + "/forecast/q/" + state + "/" + city + ".json")
     json_string = f.read().decode('utf-8')
     parsed_json = json.loads(json_string)
     forecastList = parsed_json["forecast"]["txt_forecast"]["forecastday"]
@@ -32,30 +33,30 @@ def make_forecast(city,state):
     f.close()
     return message
 
+
 def send_alerts():
-    wunderground_this_min=0
+    wunderground_this_min = 0
     cur = conn.cursor()
     cur.execute("SELECT * FROM user")
     for user in cur:
         number = str(user[0])
         city = str(user[1])
         state = str(user[2])
-        if city+state not in forecasts: #find forecasts only for new cities
-            print("Checking forecast for "+city)
-            wunderground_this_min+=1
-            if(wunderground_this_min>=10):  #Only make 10 api calls/min
+        if city + state not in forecasts:  # find forecasts only for new cities
+            print("Checking forecast for " + city)
+            wunderground_this_min += 1
+            if (wunderground_this_min >= 10):  # Only make 10 api calls/min
                 time.sleep(120)
-                wunderground_this_min=0
-            forecasts[city+state] = make_forecast(city,state)
-        #send_email(number+"@vtext.com","Test")
-        if forecasts[city+state]: #if there is thunder, message the user
-            send_email(number+"@vtext.com",forecasts[city+state])
-            #client.messages.create(to = number, from_ = config.TWILIO_NUMBER, body = forecasts[city+state])
+                wunderground_this_min = 0
+            forecasts[city + state] = make_forecast(city, state)
+        # send_email(number+"@vtext.com","Test")
+        if forecasts[city + state]:  # if there is thunder, message the user
+            send_email(number + "@vtext.com", forecasts[city + state])
+            # client.messages.create(to = number, from_ = config.TWILIO_NUMBER, body = forecasts[city+state])
 
 
-
-
-def send_email(recipient,body, user="thunderbuddyproject@gmail.com", pwd=config.EMAIL_PASSWORD, subject="ThunderBuddy"):
+def send_email(recipient, body, user="thunderbuddyproject@gmail.com", pwd=config.EMAIL_PASSWORD,
+               subject="ThunderBuddy"):
     import smtplib
 
     gmail_user = 'thunderbuddyproject@gmail.com'
@@ -75,11 +76,9 @@ def send_email(recipient,body, user="thunderbuddyproject@gmail.com", pwd=config.
         server.login(gmail_user, gmail_pwd)
         server.sendmail(FROM, TO, message)
         server.close()
-        print ('successfully sent the mail')
+        print('successfully sent the mail')
     except:
-        print ("failed to send mail")
-
+        print("failed to send mail")
 
 
 send_alerts()
-
