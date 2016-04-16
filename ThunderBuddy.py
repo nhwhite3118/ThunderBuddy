d78244050558d0f1812fdc2121190faff7be50e8
danielrobertson <danielrobertson733@gmail.com>
2016-04-16 15:23:22 -0500

    better english sentences, fixes #28

--- ThunderBuddy.py
+++ ThunderBuddy.py
@@ -7,16 +7,16 @@ import smtplib
 
 # We need to keep track of the number of unique cities we ask weatherunderground
 # for. We get 10 API calls/minute, and 500/day, so we need to keep totals
-wunderground_this_day = 0
+wundergroundThisDay = 0
 # We need to cache cities so that our usage scales with unique cities instead
 # of unique users
 forecasts = {}
 conn = pymysql.connect(host="127.0.0.1", user=config.DB_USER, passwd=config.DB_PASSWORD, db="thunderbuddy")
 
 
-def send_alerts():
+def sendAlerts():
     print("time - " + time.ctime())
-    wunderground_this_min = 0
+    wundergroundThisMinute = 0
     cur = conn.cursor()
     cur.execute("SELECT * FROM user")
 
@@ -25,38 +25,45 @@ def send_alerts():
         city = user[1]
         state = user[2]
         portal = user[3]
-        if city + state not in forecasts:  # find forecasts only for new cities
+        if city + state not in forecasts:
+            # find forecasts only for new cities
             print("Checking forecast for " + city)
-            wunderground_this_min += 1
-            if wunderground_this_min >= 10:  # Only make 10 api calls/min
+            wundergroundThisMinute += 1
+            if wundergroundThisMinute >= 10:
+                # Only make 10 api calls/min
                 time.sleep(120)
-                wunderground_this_min = 0
-            forecasts[city + state] = make_forecast(city, state)
+                wundergroundThisMinute = 0
+            forecasts[city + state] = makeForecast(city, state)
 
-        if forecasts[city + state]:  # if there is thunder, message the user
-            send_email(number + "@" + portal, forecasts[city + state])
+        if len(forecasts[city + state]) > 0:
+            # if there is thunder, message the user
+            sendEmailSms(number + "@" + portal, forecasts[city + state])
 
 
-def make_forecast(city, state):
+def makeForecast(city, state):
     # Retrieve Weather Underground Data
-    f = urlopen(
-        "http://api.wunderground.com/api/" + config.WEATHER_UNDERGROUND_API_KEY + "/forecast/q/" + state + "/" + city + ".json")
+    f = urlopen("http://api.wunderground.com/api/" + config.WEATHER_UNDERGROUND_API_KEY + "/forecast/q/" + state + "/" + city + ".json")
     jsonString = f.read().decode('utf-8')
     parsedJson = json.loads(jsonString)
     forecastList = parsedJson["forecast"]["txt_forecast"]["forecastday"]
+    thunderDays = []
 
     message = ""
     for forecastEntry in forecastList:
         fcttext = forecastEntry["fcttext"].lower()
-        fcttext_metric = forecastEntry["fcttext_metric"].lower()
-        if "thunder" in fcttext or "thunder" in fcttext_metric:
-            print("Appending to message - thunder coming on " + forecastEntry["title"] + "!")
-            message += "Thunder coming on " + forecastEntry["title"] + "!\n"
+        fcttextMetric = forecastEntry["fcttext_metric"].lower()
+        if "thunder" in fcttext or "thunder" in fcttextMetric:
+            thunderDays.append(forecastEntry["title"])
+
+    if len(thunderDays) > 0:
+        # build message and replace last comma with 'and'
+        message = "Thunder coming on " + ", ".join(thunderDays)[::-1].replace(",", "dna ", 1)[::-1]
+
     f.close()
     return message
 
 
-def send_email(recipient, body):
+def sendEmailSms(recipient, body):
     gmailUser = 'thunderbuddyproject@gmail.com'
     gmailPassword = config.EMAIL_PASSWORD
     to = recipient if type(recipient) is list else [recipient]
@@ -73,4 +80,4 @@ def send_email(recipient, body):
         print("failed to send email to - " + str(to))
 
 
-send_alerts()
+sendAlerts()
